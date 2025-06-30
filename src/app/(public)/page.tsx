886cbb23f84cbee356ff2f8c7cc41282d7216368
Albernando <alberinandocipriano@hotmail.com>
2025-06-30 20:21:12 -0300

    first commit

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Header} from "@/app/(public)/_components/header";
+import Hero from "@/app/(public)/_components/hero";
+
+export default function Home(){
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header/>
+
+            <div>
+                <Hero/>
+            </div>
+        </div>
+    )
+}
