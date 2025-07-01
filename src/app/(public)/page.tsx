6b1d4bdff548060ab33c2ab6c9c80566bda79089
Albernando <alberinandocipriano@hotmail.com>
2025-06-30 21:06:56 -0300

    Fix: footer

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {Header} from "@/app/(public)/_components/header";
 import Hero from "@/app/(public)/_components/hero";
+import Professionals from "@/app/(public)/_components/professionals";
+import Footer from "@/app/(public)/_components/footer";
 
 export default function Home(){
     return (
@@ -9,6 +11,10 @@ export default function Home(){
 
             <div>
                 <Hero/>
+
+                <Professionals />
+
+                <Footer />
             </div>
         </div>
     )
